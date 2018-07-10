25969bf37578a96bfcc42e3eb15c3b52a3e9b074
517030910409 <40875634+517030910409@users.noreply.github.com>
2018-07-10 15:13:02 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -10,12 +10,8 @@
 #define size_t unsigned int
 
 int main(int argc, char** argv) {
-    //freopen("code.in", "r", stdin);
-    //freopen("code.out","w",stdout);
     registerSet rst; 
     std::string fileName = argv[1]; 
-    //std::string fileName; 
-    //std::cin >> fileName; 
     size_t data; 
     char* text = load(fileName, rst, data); 
     instructionFetch IF(rst, text); 
@@ -33,7 +29,6 @@ int main(int argc, char** argv) {
 	ID.work(); 
 	if (!ID.lock)
 	    IF.work(); 
-	//std::cout << "IF:" << IF.lock << ' ' << IF.opt.cmd << ' ' << IF.opt.arg[0] << ' ' << IF.opt.arg[1] << ' ' << IF.opt.arg[2] << '\n'; 	std::cout << "ID:" << ID.lock << ' ' << ID.opt.cmd << ' ' << ID.opt.arg[0] << ' ' << ID.opt.arg[1] << ' ' << IF.opt.arg[2] << '\n'; 	std::cout << "EX:" << "  " << EX.opt.cmd << ' ' << EX.opt.arg[0] << ' ' << EX.opt.arg[1] << ' ' << IF.opt.arg[2] << '\n'; 	std::cout << "MA:" << "  " << MA.opt.cmd << ' ' << MA.opt.arg[0] << ' ' << MA.opt.arg[1] << ' ' << MA.opt.arg[2] << '\n'; 	std::cout << "WB:" << "  " << WB.opt.cmd << ' ' << WB.opt.arg[0] << ' ' << WB.opt.arg[1] << ' ' << WB.opt.arg[2] << '\n'; 
 	MA >> WB; 
 	EX >> MA; 
 	ID >> EX; 
